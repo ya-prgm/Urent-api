ce545e0ab103eb05c8f96ae39fd5fc6fc381f1bd
ya-prgm <ya.prgm@gmail.com>
2026-05-23 16:14:10 +0500

    Файл auth.py, передает все токены

---
+++ config.py
@@ -0,0 +1,15 @@
+# Urent API Configuration
+# Токены авторизации
+ACCESS_TOKEN = ""
+REFRESH_TOKEN = ""
+CLIENT_ID = "mobile.client.android"
+CLIENT_SECRET = ""
+DEVICE_ID = ""
+SESSION_ID = ""
+APPSFLYER_ID = ""
+USER_ID = ""
+PHONE = ""
+
+# Координаты для примера
+TYUMEN_LAT = 53.1522
+TYUMEN_LON = 75.5272
